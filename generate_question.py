34a114a16565e9a42dbeed0c9f8d4b7dad6bb77d
Paul Liao <liaoyunyang0815@gmail.com>
2024-08-14 22:32:03 +0800

    change pdf loader and adjust the prompt

--- generate_question.py
+++ generate_question.py
@@ -1,8 +1,10 @@
 """"generate question"""
 import os
+from typing import List
+from langchain.prompts import ChatPromptTemplate
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_ollama import ChatOllama
-from langchain.prompts import ChatPromptTemplate
+from pydantic import ValidationError
 
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
@@ -12,15 +14,35 @@ os.environ["LANGCHAIN_API_KEY"] = "lsv2_pt_e3c5be77de574d7bb5de07d36a8f10ce_7d71
 # Pydantic
 class Question(BaseModel):
     """Question based on the context"""
-    question: str = Field(description="Question based on excerpt or chunk from provided context, without relying on external knowledge or assumptions. Use only the information learned from the given context.")
-    source: str = Field(description="The initial chunk or excerpt that the question is based on")
 
-GENERATE_TEMPLATE = """{context}
+    question: str = Field(
+        description=(
+            "A question generated based on key sentences selected from the provided context. "
+            "The model identifies crucial information within the context, isolates the most relevant "
+            "sentences, and crafts a question that can be answered using only the content of these "
+            "key sentences. The generated question should be clear, specific, and directly related to "
+            "the extracted key sentences, ensuring that no external knowledge or assumptions are required."
+        )
+    )
+
+    key_sentences: List[str] = Field(
+        description=(
+            "A list of short, precise key sentences extracted from the provided context. Each sentence should be short, concise, "
+            "clear, and directly relevant to the answer of the generated question. The selected sentences "
+            "should collectively contain all the information necessary to infer the correct answer, without "
+            "relying on additional context or external knowledge. These sentences should be carefully chosen "
+            "to ensure they are short and precise, effectively supporting the question-answering task."
+        )
+    )
+
+
+GENERATE_TEMPLATE = """
+{context}
 
 -----------
 
-Choose a chunk or excerpt from the preceding context, formulate a thoughtful question that builds upon the chunk or excerpt you chose. Please include both your inquiry and the original
-excerpt in your response."""
+From the context provided above, carefully extract key sentences that encapsulate the most crucial information. These sentences should be as SHORT as possible, concise, clear, and directly relevant to the main ideas presented. Once you have identified these key sentences, formulate a well-considered and precise question that can be answered solely based on the information within these sentences. Ensure the question is specific, focused, and directly tied to the selected text. In your response, include both the question and the key sentences to ensure clarity and context.
+"""
 
 GENERATE_PROMPT = ChatPromptTemplate.from_template(GENERATE_TEMPLATE)
 
@@ -34,16 +56,15 @@ def generate_question(context) -> [str]:
     structured_model = model.with_structured_output(Question)
     main_chain = GENERATE_PROMPT | structured_model
 
-    output = []
-
     try:
         model_output = main_chain.invoke({'context': context})
+        output = []
         output.append(model_output.question)
-        output.append(model_output.source)
-    except Exception as e:
+        output.append('; '.join(sentence for sentence in model_output.key_sentences))
+        return output
+    except ValidationError as e:
         print(f"Validation error: {e}")
-
-    return output
+        return []
 
 
 if __name__ == "__main__":
--- load_and_split_pdf.py
+++ load_and_split_pdf.py
@@ -1,6 +1,11 @@
-from langchain_community.document_loaders import PyMuPDFLoader
+"""load and split pdf"""
+from langchain_community.document_loaders import PyPDFLoader
 
 def load_and_split_pdf(filename):
-    loader = PyMuPDFLoader(filename)
+    """main"""
+    loader = PyPDFLoader(filename)
     data = loader.load_and_split()
     return data
+
+if __name__ == "__main__":
+    print(load_and_split_pdf("data/Segment_Tree.pdf"))
